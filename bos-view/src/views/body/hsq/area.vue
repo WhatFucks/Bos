56cb5b5efdc66b94e6285c49c5a6607b5914c36d
hsq376895089 <376895089@qq.com>
2019-08-15 11:07:43 +0800

    hsq最后一次上交

--- bos-view/src/views/body/hsq/area.vue
+++ bos-view/src/views/body/hsq/area.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.name" placeholder="name" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.province" placeholder="省份" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         查询
       </el-button>
