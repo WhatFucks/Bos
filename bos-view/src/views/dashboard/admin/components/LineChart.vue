3fd251766ba0c07211c5117b5a660d54a2dd5a21
luojing <3118048570@qq.com>
2019-08-17 13:01:35 +0800

    2019-8-17 星期六 下午 13.00

--- bos-view/src/views/dashboard/admin/components/LineChart.vue
+++ bos-view/src/views/dashboard/admin/components/LineChart.vue
@@ -1,135 +1,50 @@
 <template>
-  <div :class="className" :style="{height:height,width:width}" />
+  <div>
+    <echart :dataOe ="dataOe" :dataTo="dataTo" :dataDay="dataDay" />
+  </div>
 </template>
 
 <script>
-import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
-import resize from './mixins/resize'
-
-export default {
-  mixins: [resize],
-  props: {
-    className: {
-      type: String,
-      default: 'chart'
-    },
-    width: {
-      type: String,
-      default: '100%'
-    },
-    height: {
-      type: String,
-      default: '350px'
-    },
-    autoResize: {
-      type: Boolean,
-      default: true
-    },
-    chartData: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
-    return {
-      chart: null
-    }
-  },
-  watch: {
-    chartData: {
-      deep: true,
-      handler(val) {
-        this.setOptions(val)
+  import Echart from '@/components/TwoEcharts'
+  import {echartsList } from '@/api/reportforms/reportforms'
+  export default {
+    name: 'Charts',
+    data () {
+      return {
+        dataOe: [],
+        dataTo: [],
+        dataThree:[],
+        dataDay:[],
+        typeBoolean: true,
+        date: ''
       }
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.initChart()
-    })
-  },
-  beforeDestroy() {
-    if (!this.chart) {
-      return
-    }
-    this.chart.dispose()
-    this.chart = null
-  },
-  methods: {
-    initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
-      this.setOptions(this.chartData)
     },
-    setOptions({ expectedData, actualData } = {}) {
-      this.chart.setOption({
-        xAxis: {
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          boundaryGap: false,
-          axisTick: {
-            show: false
-          }
-        },
-        grid: {
-          left: 10,
-          right: 10,
-          bottom: 20,
-          top: 30,
-          containLabel: true
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross'
-          },
-          padding: [5, 10]
-        },
-        yAxis: {
-          axisTick: {
-            show: false
-          }
-        },
-        legend: {
-          data: ['expected', 'actual']
-        },
-        series: [{
-          name: 'expected', itemStyle: {
-            normal: {
-              color: '#FF005A',
-              lineStyle: {
-                color: '#FF005A',
-                width: 2
-              }
-            }
-          },
-          smooth: true,
-          type: 'line',
-          data: expectedData,
-          animationDuration: 2800,
-          animationEasing: 'cubicInOut'
-        },
-        {
-          name: 'actual',
-          smooth: true,
-          type: 'line',
-          itemStyle: {
-            normal: {
-              color: '#3888fa',
-              lineStyle: {
-                color: '#3888fa',
-                width: 2
-              },
-              areaStyle: {
-                color: '#f3f8ff'
-              }
-            }
-          },
-          data: actualData,
-          animationDuration: 2800,
-          animationEasing: 'quadraticOut'
-        }]
-      })
+    mounted() {
+      this.getList()
+    },
+    methods:{
+      getList() {
+        echartsList().then(response => {
+          this.dataOe= response.data.dataOne
+          this.dataTo=response.data.dataTwo
+          this.dataDay=response.data.dataDay
+        })
+
+      },
+    },
+    computed: {
+      type: function () {
+        return this.typeBoolean ? 'bar' : 'line'
+      }
+    },
+    components: {
+      Echart
     }
   }
-}
 </script>
+
+<style scoped lang="scss">
+  .filter-charts {
+    border: 1px solid rgba(155, 155, 155, 0.2)
+  }
+</style>
